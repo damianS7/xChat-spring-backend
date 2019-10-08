24babd84801721addd16819922c46d8cb031c36b
damianS7 <josepwnz@gmail.com>
2019-10-08 22:12:28 +0200

    Gitignore actualizado para descartar configuraciones de  Eclipse y Vscode

---
+++ src/main/java/com/chat/client/ClientApplication.java
@@ -0,0 +1,5 @@
+package com.chat.client;
+
+public class ClientApplication {
+
+}
---
+++ src/main/java/com/chat/server/ServerApplication.java
@@ -0,0 +1,8 @@
+package server;
+
+
+public class ServerApplication {
+    public static void main(String[] args) {
+        System.out.println("Running server ...");
+    }
+}
