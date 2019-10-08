b1b999199bbf32f6baa9ef808797edf9943b2788
damianS7 <josepwnz@gmail.com>
2019-10-08 22:13:59 +0200

    Revert "Gitignore actualizado para descartar configuraciones de  Eclipse y Vscode"
    This reverts commit 24babd84801721addd16819922c46d8cb031c36b.

--- src/main/java/com/chat/client/ClientApplication.java
+++
@@ -1,5 +0,0 @@
-package com.chat.client;
-
-public class ClientApplication {
-
-}
--- src/main/java/com/chat/server/ServerApplication.java
+++
@@ -1,8 +0,0 @@
-package server;
-
-
-public class ServerApplication {
-    public static void main(String[] args) {
-        System.out.println("Running server ...");
-    }
-}
