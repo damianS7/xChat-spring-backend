91c8a7f5852339140bcaa95c9cbf316f7f50343b
damianS7 <josepwnz@gmail.com>
2019-10-10 18:22:49 +0200

    Metodo main actualizado

--- src/main/java/com/chat/client/ClientApplication.java
+++ src/main/java/com/chat/client/ClientApplication.java
@@ -1,7 +1,24 @@
 package com.chat.client;
 
-public class ClientApplication {
-    public static void main(String[] args) {
+import java.awt.EventQueue;
+import java.io.File;
 
+import com.chat.client.ui.ClientUI;
+
+public class ClientApplication {
+    public static ClientUI ui;
+    public static ClientChat client;
+    public static Preferences config;
+
+    public static void main(String[] args) {
+        config = new Preferences(new File(""));
+        client = new ClientChat();
+
+        // Inicio de la interfaz grafica en un hilo separado
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                ui = new ClientUI();
+            }
+        });
     }
 }
--- src/main/java/com/chat/server/ServerApplication.java
+++ src/main/java/com/chat/server/ServerApplication.java
@@ -1,7 +1,17 @@
 package com.chat.server;
 
-public class ServerApplication {
-    public static void main(String[] args) {
+import com.chat.ui.ServerUI;
+import java.awt.EventQueue;
 
+public class ServerApplication {
+    public static ServerUI ui;
+
+    public static void main(String[] args) {
+        // Inicio de la interfaz grafica en un hilo separado
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                ui = new ServerUI();
+            }
+        });
     }
 }
