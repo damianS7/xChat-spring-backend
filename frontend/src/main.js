229d09ab881b9aeaf18eac9fee777b74a5d85363
damianS7 <josepwnz@gmail.com>
2021-06-27 00:03:08 +0200

    ..

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,18 +1,14 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
-
-Vue.use(BootstrapVue)
-
 import router from './router/index'
 import store from './store/index'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 //require('./bootstrap');
-
+Vue.use(BootstrapVue)
 window.Vue = require('vue');
-
 
 Vue.config.productionTip = false
 new Vue({
