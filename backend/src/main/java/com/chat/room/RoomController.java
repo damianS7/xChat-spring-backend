9198939c3923be4f84eb560ff5f95585c84118b7
damianS7 <josepwnz@gmail.com>
2021-06-26 16:15:05 +0200

    Prueba api funcionando en /api/rooms

--- backend/src/main/java/com/chat/room/RoomController.java
+++ backend/src/main/java/com/chat/room/RoomController.java
@@ -1,5 +1,22 @@
 package com.chat.room;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/rooms")
+@CrossOrigin
 public class RoomController {
 
+	@GetMapping("/")
+    public String index() {
+        return "rooms response!";
+    }
+	
+	@GetMapping("/myrooms")
+    public String index2() {
+        return "another response!";
+    }
 }
