da802a73fea24813c849a7df3c8ad0c7ca9e2cbc
damian <git2507>
2021-07-04 16:31:47 +0200

    Agregado campo ROL. Modificado campo name por username. Implmentado la interfaz UserDetails que contiene datos de autorizacion.

--- src/main/java/com/chat/user/User.java
+++ src/main/java/com/chat/user/User.java
@@ -1,29 +1,40 @@
 package com.chat.user;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
-    private String name;
+    private String username;
     @Column
     private String email;
     @Column
     private String password;
 
+    @Enumerated(EnumType.STRING)
+    @Column
+    private UserRole role;
+
     public User() {
         // ...
     }
 
-    public User(Long id, String name, String email, String password) {
+    public User(Long id, String username, String email, String password) {
         this.id = id;
-        this.name = name;
+        this.username = username;
         this.email = email;
         this.password = password;
+        this.role = UserRole.USER;
     }
 
     public Long getId() {
@@ -32,14 +43,6 @@ public class User {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getEmail() {
@@ -56,5 +59,45 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority =
+                new SimpleGrantedAuthority(UserRole.MOD.name());
+        return Collections.singletonList(authority);
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
     }
 }
---
+++ src/main/java/com/chat/user/UserRole.java
@@ -0,0 +1,5 @@
+package com.chat.user;
+
+public enum UserRole {
+    USER, MOD
+}
