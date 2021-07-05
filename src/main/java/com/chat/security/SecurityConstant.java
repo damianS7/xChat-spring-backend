5395a62cd2f3935d2d941d3fce7bd91768e60d5b
damian <git2507>
2021-07-05 20:20:04 +0200

    Uso de filtros para el login. Configuracion CORS para permitir peticiones del cliente.

---
+++ src/main/java/com/chat/security/SecurityConstant.java
@@ -0,0 +1,8 @@
+package com.chat.security;
+
+public class SecurityConstant {
+    public static final String SECRET_KEY = "123456";
+    public static final String HEADER_AUTHORIZACION_KEY = "Authorization";
+    public static final String TOKEN_BEARER_PREFIX = "Bearer ";
+    public static final String ISSUER_INFO = "http://localhost:8888/";
+}
--- src/main/java/com/chat/security/WebSecurityConfig.java
+++ src/main/java/com/chat/security/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.chat.security;
 
+import com.chat.authentication.AuthenticationFilter;
+import com.chat.authentication.AuthorizationFilter;
 import com.chat.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,7 +11,11 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -26,19 +32,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/registration", "/login/**")
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().cors()
+                .and().csrf().disable()
+                .authorizeRequests().antMatchers("/login/**", "/registration/**")
+                //.authorizeRequests().antMatchers(HttpMethod.POST, LOGIN_URL).permitAll()
                 .permitAll()
-                .anyRequest()
-                .authenticated().and()
-                .formLogin();
-
+                .anyRequest().authenticated().and()
+                .addFilter(new AuthenticationFilter(authenticationManager()))
+                .addFilter(new AuthorizationFilter(authenticationManager()));
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    protected void configure(AuthenticationManagerBuilder auth) {
+        // Se define la clase que recupera los usuarios y el algoritmo para procesar las passwords
         auth.authenticationProvider(daoAuthenticationProvider());
     }
 
@@ -49,5 +57,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         provider.setPasswordEncoder(bCryptPasswordEncoder);
         provider.setUserDetailsService(userService);
         return provider;
+    }
+
+    /*
+     * Debido a que el backend y el frontend corren en puertos diferentes
+     * por defecto Spring aplica CORS, el cual solo permite peticiones
+     * del mismo origen (esto incluye los puertos tambien) por lo que es necesario
+     * configurar CORS para permitir el puerto donde se ejecuta el frontend
+     */
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.applyPermitDefaultValues();
+        //configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
+        //configuration.setAllowedMethods(Arrays.asList("GET", "OPTIONS", "POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
